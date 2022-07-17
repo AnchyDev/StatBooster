b6e6ceb74b8fcfa62991d7616b0882f73e23ebf8
AnchyDev <lewienato@gmail.com>
2022-07-17 14:14:30 +0800

    Added required hooks & initialized config settings

--- src/StatBooster.cpp
+++ src/StatBooster.cpp
@@ -12,6 +12,30 @@ public:
 			ChatHandler(player->GetSession()).SendSysMessage("This server is running the StatBooster module.");
 		}
 	}
+
+    void OnLootItem(Player* player, Item* item, uint32 /*count*/, ObjectGuid /*lootguid*/) override
+    {
+        if (BoosterConfig.OnLootItemEnable)
+        {
+
+        }
+    }
+
+    void OnQuestRewardItem(Player* player, Item* item, uint32 /*count*/) override
+    {
+        if (BoosterConfig.OnQuestRewardItemEnable)
+        {
+
+        }
+    }
+
+    void OnCreateItem(Player* player, Item* item, uint32 /*count*/) override
+    {
+        if (BoosterConfig.OnCraftItemEnable)
+        {
+
+        }
+    }
 };
 
 void AddSCStatBoosterScripts()
@@ -22,6 +46,12 @@ void AddSCStatBoosterScripts()
     }
 	
 	BoosterConfig.OnLoginEnable = sConfigMgr->GetOption<bool>("StatBooster.OnLoginEnable", true);
+    BoosterConfig.OnLootItemEnable = sConfigMgr->GetOption<bool>("StatBooster.OnLootItemEnable", true);
+    BoosterConfig.OnQuestRewardItemEnable = sConfigMgr->GetOption<bool>("StatBooster.OnQuestRewardItemEnable", true);
+    BoosterConfig.OnCraftItemEnable = sConfigMgr->GetOption<bool>("StatBooster.OnCraftItemEnable", true);
+
+    BoosterConfig.MinQuality = sConfigMgr->GetOption<bool>("StatBooster.MinQuality", ITEM_QUALITY_UNCOMMON);
+    BoosterConfig.MaxQuality = sConfigMgr->GetOption<bool>("StatBooster.MaxQuality", ITEM_QUALITY_EPIC);
 	
     new StatBoosterPlayer();
 }
