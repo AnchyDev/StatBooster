afad10be9d215669b3a5f736fb75c08dcaf791af
AnchyDev <lewienato@gmail.com>
2022-07-27 02:38:11 +0100

    Delete unused SQL & updated SQL to include ClassMask column.

--- sql/world/base/development.sql
+++ sql/world/base/development.sql
@@ -20,30 +20,31 @@ CREATE TABLE IF NOT EXISTS `statbooster_enchant_template` (
   `iLvlMin` int DEFAULT NULL,
   `iLvlMax` int DEFAULT NULL,
   `RoleMask` int DEFAULT NULL,
-  `Description` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `Note` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL
+  `ClassMask` int DEFAULT NULL,
+  `Description` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Note` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table acore_world.statbooster_enchant_template: ~8 rows (approximately)
-INSERT INTO `statbooster_enchant_template` (`Id`, `iLvlMin`, `iLvlMax`, `RoleMask`, `Description`, `Note`) VALUES
-	(68, 1, 20, 3, '+1 Strength', 'TANK/PHYS'),
-	(74, 1, 20, 7, '+1 Agility', 'TANK/PHYS/HYBRID'),
-	(79, 1, 20, 12, '+1 Intellect', 'HYBRID/SPELL'),
-	(206, 1, 20, 8, '+1 Spell Power', 'SPELL'),
-	(66, 1, 20, 0, '+1 Stamina', 'ALL'),
-	(82, 1, 20, 8, '+1 Spirit', 'SPELL'),
-	(75, 10, 30, 7, '+2 Agility', 'TANK/PHYS/HYBRID'),
-	(69, 10, 30, 3, '+2 Strength', 'TANK/PHYS'),
-	(80, 10, 30, 12, '+2 Intellect', 'HYBRID/SPELL'),
-	(72, 10, 30, 0, '+2 Stamina', 'ALL'),
-	(83, 10, 30, 8, '+2 Spirit', 'SPELL'),
-	(207, 10, 30, 8, '+2 Spell Power', 'SPELL'),
-	(76, 20, 40, 7, '+3 Agility', 'TANK/PHYS/HYBRID'),
-	(81, 20, 40, 12, '+3 Intellect', 'HYBRID/SPELL'),
-	(73, 20, 40, 0, '+3 Stamina', 'ALL'),
-	(64, 20, 40, 8, '+3 Spirit', 'SPELL'),
-	(70, 20, 40, 3, '+3 Strength', 'TANK/PHYS'),
-	(2910, 20, 40, 8, '+3 Spell Power', 'SPELL');
+-- Dumping data for table acore_world.statbooster_enchant_template: ~18 rows (approximately)
+INSERT INTO `statbooster_enchant_template` (`Id`, `iLvlMin`, `iLvlMax`, `RoleMask`, `ClassMask`, `Description`, `Note`) VALUES
+	(68, 1, 20, 3, 0, '+1 Strength', 'TANK/PHYS'),
+	(74, 1, 20, 7, 0, '+1 Agility', 'TANK/PHYS/HYBRID'),
+	(79, 1, 20, 12, 0, '+1 Intellect', 'HYBRID/SPELL'),
+	(206, 1, 20, 8, 0, '+1 Spell Power', 'SPELL'),
+	(66, 1, 20, 0, 0, '+1 Stamina', 'ALL'),
+	(82, 1, 20, 8, 0, '+1 Spirit', 'SPELL'),
+	(75, 10, 30, 7, 0, '+2 Agility', 'TANK/PHYS/HYBRID'),
+	(69, 10, 30, 3, 0, '+2 Strength', 'TANK/PHYS'),
+	(80, 10, 30, 12, 0, '+2 Intellect', 'HYBRID/SPELL'),
+	(72, 10, 30, 0, 0, '+2 Stamina', 'ALL'),
+	(83, 10, 30, 8, 0, '+2 Spirit', 'SPELL'),
+	(207, 10, 30, 8, 0, '+2 Spell Power', 'SPELL'),
+	(76, 20, 40, 7, 0, '+3 Agility', 'TANK/PHYS/HYBRID'),
+	(81, 20, 40, 12, 0, '+3 Intellect', 'HYBRID/SPELL'),
+	(73, 20, 40, 0, 0, '+3 Stamina', 'ALL'),
+	(64, 20, 40, 8, 0, '+3 Spirit', 'SPELL'),
+	(70, 20, 40, 3, 0, '+3 Strength', 'TANK/PHYS'),
+	(2910, 20, 40, 8, 0, '+3 Spell Power', 'SPELL');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
