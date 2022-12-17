5780e4fffdbe3995355fd789af57dbc6cc299e4c
AnchyDev <lewienato@gmail.com>
2022-12-18 06:48:48 +1100

    Removed deletes from SQL.

--- data/sql/db-world/StatBooster.sql
+++ data/sql/db-world/StatBooster.sql
@@ -1,7 +1,5 @@
-DELETE FROM `command` WHERE name = 'sb';
 INSERT INTO `command`(`name`, `security`, `help`) VALUES ('sb', 3, 'Syntax: .sb - type .sb to list subcommands.');
 
-DELETE FROM `command` WHERE name = "sb additem";
 INSERT INTO `command`(`name`, `security`, `help`) VALUES ('sb additem', 3, 'Syntax: .sb additem <itemid> <count> - tries to add an enchanted item to targeted player.');
 
 CREATE TABLE IF NOT EXISTS `statbooster_enchant_template` (
@@ -15,7 +13,6 @@ CREATE TABLE IF NOT EXISTS `statbooster_enchant_template` (
   `Note` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-DELETE FROM `statbooster_enchant_template`;
 INSERT INTO `statbooster_enchant_template` (`Id`, `iLvlMin`, `iLvlMax`, `RoleMask`, `ClassMask`, `SubClassMask`, `Description`, `Note`) VALUES
 	(68, 1, 20, 3, 0, 0, '+1 Strength', 'TANK/PHYS - ALL - ALL'),
 	(74, 1, 20, 7, 0, 0, '+1 Agility', 'TANK/PHYS/HYBRID - ALL - ALL'),
@@ -71,7 +68,6 @@ CREATE TABLE IF NOT EXISTS `statbooster_enchant_scores` (
   `note` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-DELETE FROM `statbooster_enchant_scores`;
 INSERT INTO `statbooster_enchant_scores` (`mod_type`, `mod_id`, `subclass`, `tank_score`, `phys_score`, `spell_score`, `hybrid_score`, `note`) VALUES
 	(0, 44, 0, 1, 2, 0, 1, 'ITEM_MOD_ARMOR_PENETRATION_RATING- ALL'),
 	(0, 38, 0, 1, 2, 0, 1, 'ITEM_MOD_ATTACK_POWER - ALL'),
